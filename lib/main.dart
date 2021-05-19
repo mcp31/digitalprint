a62e6e6c956c19fbe600e74bda04815a002631d1
mcp31 <mcplana31@gmail.com>
2021-05-19 15:09:32 -0500

    Added comments in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,8 @@ class MyApp extends StatelessWidget {
       /*
       Currently, there's no way to execute different screens
       at the command line
+            -Maybe have two different types of main.dart?
+            https://stackoverflow.com/questions/55004302/how-do-you-pass-arguments-from-command-line-to-main-in-flutter-dart
        */
 
       //home: MyHomePage(),
